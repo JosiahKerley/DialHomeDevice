e4804fd107bdbd3e4d3bd724a01f4d189e87867c
Josiah Kerley <josiahkerley@gmail.com>
2015-01-04 22:05:59 -0700

    fixed session backtick

--- dhd.sh
+++ dhd.sh
@@ -41,7 +41,7 @@ then
   then
     echo "Format: dhd exec clientsessionname"
   else
-    file=session2hostname $2
+    file=`session2hostname $2`
     if [ -f "$file" ]
     then
       ssh localhost -p `cat $file` $3 $4 $5 $6 $7 $8 $9
